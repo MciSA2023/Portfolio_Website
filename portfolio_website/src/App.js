baccaf05f7fa054b15680ff7c84889d9d0b32642
Adhar Singh <130926943+MciSA2023@users.noreply.github.com>
2024-01-07 19:51:11 +0100

    TopBar initial

--- portfolio_website/src/App.js
+++ portfolio_website/src/App.js
@@ -1,10 +1,12 @@
 import logo from './logo.svg';
 import './App.css';
+import TopBar from './components/TopBar';
 
 function App() {
   return (
     <div className="App">
-      <h1>Hello, World!</h1>
+      <TopBar />
+      
     </div>
   );
 }
