44c862e00c6c54fca13272360d652489ab46c422
Adhar Singh <130926943+MciSA2023@users.noreply.github.com>
2024-01-12 12:47:26 +0100

    Header done. About in building atm

--- portfolio_website/src/components/About.js
+++ portfolio_website/src/components/About.js
@@ -3,7 +3,19 @@ import './About.css';
 function About() {
 
 return(
-    <div classname="about_mutter_container"></div>
+    <div classname="about_mutter_container" id="about_mutter">
+      <div className="about_grid_container">
+        <div className="about_grid">
+            <div class="grid_item item1">Item 1</div>
+            <div class="grid_item item2">Item 2</div>
+            <div class="grid_item item3">Item 3</div>
+            <div class="grid_item">Item 4</div>
+            <div class="grid_item">Item 5</div>
+            <div class="grid_item">Item 6</div>
+        </div>
+      </div>
+
+    </div>
 
 );
 
--- portfolio_website/src/components/TopBar.js
+++ portfolio_website/src/components/TopBar.js
@@ -5,11 +5,11 @@ function TopBar() {
     return (
       <div className="topbar_container">
         <div className = "topbarleft_placer">
-            <div className="topbarleft_item">Logo</div>
+            <div className="topbarleft_item"></div>
         </div>
         <nav class="topbarright_placer">
         <a class="topbarright_item" href="#">Home</a>
-        <a class="topbarright_item" href="#">About</a>
+        <a class="topbarright_item" href="#about_mutter">About</a>
         <a class="topbarright_item" href="#">Projects</a>
         <a class="topbarright_item" href="#">Contact</a>
         </nav>
