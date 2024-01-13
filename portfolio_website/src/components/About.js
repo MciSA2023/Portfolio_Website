f89ebeb60ad7a787445e7d03ecd2d9bb069697ef
Adhar Singh <130926943+MciSA2023@users.noreply.github.com>
2024-01-13 14:09:27 +0100

    Bild eingefügt. (Refactoring Visuell)

--- portfolio_website/src/components/About.js
+++ portfolio_website/src/components/About.js
@@ -3,15 +3,23 @@ import './About.css';
 function About() {
 
 return(
-    <div classname="about_mutter_container" id="about_mutter">
-      <div className="about_grid_container">
-        <div className="about_grid">
-            <div class="grid_item item1">Item 1</div>
-            <div class="grid_item item2">Item 2</div>
+    <div class="about_mutter_container" id="about_mutter">
+      <div class="about_grid_container">
+        <div class="about_grid">
+            <div class="grid_item item1">
+              <div class="about_bild_container">
+              <img class="profil_bild"src={require ("./resourcen/DSC09594.JPG")} alt="Profilbild" /> 
+              </div>
+            </div>
+            <div class="grid_item item2">
+             <p> printf("Hello World")
+              My Name ist Adhar Singh, I am 22 Years old and I am currently studying Digital Business Software Engenieering at the University at the MCI in Innsbruck.</p>
+            </div>
             <div class="grid_item item3">Item 3</div>
             <div class="grid_item">Item 4</div>
             <div class="grid_item">Item 5</div>
             <div class="grid_item">Item 6</div>
+            <div class="grid_item">Item 7</div>
         </div>
       </div>
 
