815282eb38995038a2deaf085f05168a38b065d8
Adhar Singh <130926943+MciSA2023@users.noreply.github.com>
2024-01-29 12:37:07 +0100

    Card animation and first card Picture

--- portfolio_website/src/components/Projects.jsx
+++ portfolio_website/src/components/Projects.jsx
@@ -7,7 +7,8 @@ function Projects() {
     <section className="projects_mutter_container">
       <div className="projects_container">
         <div className='projects_project_container'>
-                <Projekt preViewurl={require ("./resourcen/DSC09594.JPG")}/>
+                <Projekt preViewurl={require ("./resourcen/App_Bild_preview.png")}
+                imageUrl={require ("./resourcen/App_Bild_Image.png")}/>
                 <Projekt preViewurl={require ("./resourcen/DSC09594.JPG")}/>
                 <Projekt preViewurl={require ("./resourcen/DSC09594.JPG")}/>
             </div>
